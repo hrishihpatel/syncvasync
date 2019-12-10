bde7c054d1735c38123dc6095165defd3081ddde
Hrishikesh Patel <hrishihpatel@gmail.com>
2019-12-09 19:45:16 -0600

    initial checkin

---
+++ syncvasync/Controllers/SyncVAsyncController.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Threading;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace syncvasync.Controllers
+{
+    [Route("api/[controller]")]
+    public class SyncVAsyncController : Controller
+    {
+        private readonly string _connectionString;
+        public SyncVAsyncController(IConfiguration configuration) => _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        [HttpGet("sync")]
+        public IActionResult SyncGet()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                connection.Execute("WAITFOR DELAY '00:00:02';");
+            }
+            return Ok(GetThreadInfo());
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> AsyncGet()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                await connection.ExecuteAsync("WAITFOR DELAY '00:00:02';");
+            }
+            return Ok(GetThreadInfo());
+        }
+
+        private dynamic GetThreadInfo()
+        {
+            int availableWorkerThreads;
+            int availableAsyncIOThreads;
+            ThreadPool.GetAvailableThreads(out availableWorkerThreads, out availableAsyncIOThreads);
+
+            return new { AvailableAsyncIOThreads = availableAsyncIOThreads, AvailableWorkerThreads = availableWorkerThreads };
+        }
+    }
+}
---
+++ syncvasync/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace syncvasync
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int processorCounter = Environment.ProcessorCount;
+            bool success = ThreadPool.SetMaxThreads(processorCounter, processorCounter);
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
